875725557f6e7e72f38048cbc6478bfe5050c123
allegro25 <alexandre.allegro@gmail.com>
2021-03-27 17:13:18 +0000

    tirei caralho do mster

--- src/containers/App.js
+++ src/containers/App.js
@@ -58,7 +58,7 @@ class App extends Component {
 		} else {
 			return(
 				<div className="tc">
-					<h1 className="f1">Robofriends o caralho</h1>
+					<h1 className="f1">Robofriends</h1>
 					<SearchBox searchChange={this.onSearchChange}/>
 					<Scroll>
 						<ErrorBoundry>
